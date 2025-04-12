85f7b505c0960278da6d6e296cc6550f85fd4d0b
gurbur <jihwan7577@gmail.com>
2025-04-12 20:34:39 +0900

    Feat: topic_table.h

---
+++ include/topic_table.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <netinet/in.h>
+#include <stddef.h>
+
+
+typedef struct Subscriber {
+	struct sockaddr_in addr;
+	struct Subscriber* next;
+} Subscriber;
+
+typedef struct SubscriberList {
+	Subscriber* head;
+	size_t count;
+} SubscriberList;
+
+// initilize/destroy topic table
+void init_topic_table(void);
+void free_topic_table(void);
+
+// add/delete subscriber
+int subscribe_topic(const char* topic_str, const struct sockaddr_in* subscriber);
+int unsubscribe_topic(const char* topic_str, const struct sockaddr_in* subscriber);
+
+// get subscriber list of given topic
+SubscriberList* get_matching_subscribers(const char* topic_str);
+
+// for utilities
+void print_topic_tree(void); // for debugging
+void free_subscriber_list(SubscriberList* list); // free list
+
