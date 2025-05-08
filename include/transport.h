1017fa7637f12f133db9e9df99cb015b1dd0e98e
gurbur <gurbur331@ajou.ac.kr>
2025-05-08 16:32:39 +0900

    Refactor: refactor name in transport.h

--- include/transport.h
+++ include/transport.h
@@ -2,13 +2,13 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "nano_msg.h"
+#include "slim_msg.h"
 
 int init_udp_socket(const char* bind_ip, uint16_t port);
 
-int send_message(int sockfd, const struct sockaddr* dest_addr, socklen_t addrlen, const nano_msg_header_t* header, const void* payload);
+int send_message(int sockfd, const struct sockaddr* dest_addr, socklen_t addrlen, const slim_msg_header_t* header, const void* payload);
 
-int recv_message(int sockfd, nano_msg_header_t* out_header, void* out_payload, size_t max_payload_len, struct sockaddr* from_addr, socklen_t* from_len);
+int recv_message(int sockfd, slim_msg_header_t* out_header, void* out_payload, size_t max_payload_len, struct sockaddr* from_addr, socklen_t* from_len);
 
 void enable_transport_debug(bool enable);
 
