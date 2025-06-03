5c6d7f670ffd7fa4179b31dba8601f530c5f059d
gurbur <gurbur331@ajou.ac.kr>
2025-06-03 21:09:15 +0900

    Hotfix: add ip selection to test clients

--- test/test_perf_qos0.c
+++ test/test_perf_qos0.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "../include/slimmq_client.h"
 #include "../include/slim_msg.h"
 
 #define COUNT 1000
 
-int main() {
-    slimmq_client_t* client = slimmq_connect("127.0.0.1", 9000);
-    if (!client) {
-        fprintf(stderr, "Failed to connect to broker\n");
-        return 1;
-    }
+int main(int argc, char* argv[]) {
+	const char* ip = "127.0.0.1";
+	int port = 9000;
 
-    slimmq_set_qos(client, QOS_AT_MOST_ONCE);
+	for (int i = 1; i < argc - 1; i++) {
+		if (strcmp(argv[i], "-ip") == 0) {
+			ip = argv[i + 1];
+		} else if (strcmp(argv[i], "-p") == 0) {
+			port = atoi(argv[i + 1]);
+		}
+	}
 
-    for (int i = 0; i < COUNT; i++) {
-        char msg[64];
-        snprintf(msg, sizeof(msg), "qos0-message-%d", i);
-        slimmq_publish(client, "test/perf", msg, strlen(msg));
-    }
+	printf("[INFO] Connecting to %s:%d...\n", ip, port);
 
-    printf("QoS 0: Sent %d messages (no confirmation)\n", COUNT);
-    slimmq_close(client);
-    return 0;
+	slimmq_client_t* client = slimmq_connect(ip, (uint16_t)port);
+	if (!client) {
+		fprintf(stderr, "Failed to connect to broker\n");
+		return 1;
+	}
+
+	slimmq_set_qos(client, QOS_AT_MOST_ONCE);
+
+	for (int i = 0; i < COUNT; i++) {
+		char msg[64];
+		snprintf(msg, sizeof(msg), "qos0-message-%d", i);
+		slimmq_publish(client, "test/perf", msg, strlen(msg));
+	}
+
+	printf("QoS 0: Sent %d messages (no confirmation)\n", COUNT);
+	slimmq_close(client);
+	return 0;
 }
 
--- test/test_perf_qos2.c
+++ test/test_perf_qos2.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "../include/slimmq_client.h"
 #include "../include/slim_msg.h"
 
 #define COUNT 1000
 
-int main() {
-    slimmq_client_t* client = slimmq_connect("127.0.0.1", 9000);
-    if (!client) {
-        fprintf(stderr, "Failed to connect to broker\n");
-        return 1;
-    }
+int main(int argc, char* argv[]) {
+	const char* ip = "127.0.0.1";
+	int port = 9000;
 
-    slimmq_set_qos(client, QOS_EXACTLY_ONCE);
-    slimmq_set_retry_policy(client, 1000, 5);
+	for (int i = 1; i < argc - 1; i++) {
+		if (strcmp(argv[i], "-ip") == 0) {
+			ip = argv[i + 1];
+		} else if (strcmp(argv[i], "-p") == 0) {
+			port = atoi(argv[i + 1]);
+		}
+	}
 
-    for (int i = 0; i < COUNT; i++) {
-        char msg[64];
-        snprintf(msg, sizeof(msg), "qos2-message-%d", i);
-        slimmq_publish(client, "test/perf", msg, strlen(msg));
-    }
+	printf("[INFO] Connecting to %s:%d...\n", ip, port);
 
-    printf("QoS 2: Sent %d messages with exactly-once delivery\n", COUNT);
-    slimmq_close(client);
-    return 0;
+	slimmq_client_t* client = slimmq_connect(ip, (uint16_t)port);
+
+	if (!client) {
+		fprintf(stderr, "Failed to connect to broker\n");
+		return 1;
+	}
+
+	slimmq_set_qos(client, QOS_EXACTLY_ONCE);
+	slimmq_set_retry_policy(client, 1000, 5);
+
+	for (int i = 0; i < COUNT; i++) {
+		char msg[64];
+		snprintf(msg, sizeof(msg), "qos2-message-%d", i);
+		slimmq_publish(client, "test/perf", msg, strlen(msg));
+	}
+
+	printf("QoS 2: Sent %d messages with exactly-once delivery\n", COUNT);
+	slimmq_close(client);
+	return 0;
 }
 
